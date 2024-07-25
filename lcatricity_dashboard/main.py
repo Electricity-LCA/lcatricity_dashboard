138eec9e99e0bbee51d95ed9c16eff8c6a7d6e24
artur <artur.do@proton.me>
2024-07-25 09:38:03 +0200

    Set page title

--- lcatricity_dashboard/main.py
+++ lcatricity_dashboard/main.py
@@ -12,6 +12,7 @@ from get_common_data import load_common_data_from_db
 
 
 def main():
+    st.set_page_config(page_title="LCAtricty")
     logging.basicConfig(level=logging.DEBUG)
 
 
@@ -39,14 +40,16 @@ def main():
 
     st.text(f'Connected to : {ELEC_LCA_API_URL}')
 
-    st.title('Electricity LCA Dashboard')
+    st.title('LCAtricity Dashboard')
 
     # Show calendar with data points per day
+    st.subheader("Choose a region")
     region_code = st.selectbox(label='Region',options=cache.regions.Code)
 
-    st.subheader('Electricity generation')
 
     if region_code:
+        st.subheader('Choose time period')
+
         calendar_response_1 = httpx.get(f'{ELEC_LCA_API_URL}/datapoints_count_by_day', params={'region_code': region_code})
 
         try:
@@ -86,7 +89,7 @@ def main():
                     generation_df_w_gen_names.drop(['Id'], axis=1, inplace=True)
                     generation_df_w_gen_names.rename({'Name': 'ElectricityType'}, axis=1, inplace=True)
 
-
+                    st.subheader('Electricty Generation')
                     st.area_chart(generation_df_w_gen_names, x='DateStamp', y='AggregatedGeneration', color='ElectricityType')
                     with st.expander('Show generation data'):
                         st.dataframe(generation_df_w_gen_names)
@@ -94,44 +97,44 @@ def main():
             st.text(f'No data for {region_code}, try a different region code')
 
 
-    st.subheader('Environmental Impacts')
-    st.text('Note that environmental impacts only cover generation of electricity that has known type. Electricity production that is classifier as "Unknown / not specified" in the chart above is ommited from calculation ')
-    impact_category_tabs = st.tabs([x for x in cache.impact_categories['Name']])
-    for i, tab in enumerate(impact_category_tabs):
-        with tab:
-            impact_category_name = cache.impact_categories.loc[i,'Name']
-            st.text(impact_category_name)
-            calc_params = {
-                'date_start': start_date,
-                'region_code': region_code,
-                'impact_category_id': i+1
-            }
-            impact_calc_response = httpx.get(f'{ELEC_LCA_API_URL}/calculate', params=calc_params)
-            if impact_calc_response.status_code >= 300 or impact_calc_response.status_code < 200:
-                st.text(impact_calc_response.text)
-                logging.warning(f'Calculation not successful. Status code: {impact_calc_response.status_code}')
-                st.text('Calculation Error')
-                return
+        st.subheader('Environmental Impacts')
+        st.text('Note that environmental impacts only cover generation of electricity that has known type. Electricity production that is classifier as "Unknown / not specified" in the chart above is ommited from calculation ')
+        impact_category_tabs = st.tabs([x for x in cache.impact_categories['Name']])
+        for i, tab in enumerate(impact_category_tabs):
+            with tab:
+                impact_category_name = cache.impact_categories.loc[i,'Name']
+                st.text(impact_category_name)
+                calc_params = {
+                    'date_start': start_date,
+                    'region_code': region_code,
+                    'impact_category_id': i+1
+                }
+                impact_calc_response = httpx.get(f'{ELEC_LCA_API_URL}/calculate', params=calc_params)
+                if impact_calc_response.status_code >= 300 or impact_calc_response.status_code < 200:
+                    st.text(impact_calc_response.text)
+                    logging.warning(f'Calculation not successful. Status code: {impact_calc_response.status_code}')
+                    st.text('Calculation Error')
+                    return
 
-            impact_results_json = impact_calc_response.json()
+                impact_results_json = impact_calc_response.json()
 
-            impact_results_df = pd.DataFrame.from_dict(impact_results_json)
+                impact_results_df = pd.DataFrame.from_dict(impact_results_json)
 
-            if impact_results_df.empty:
-                st.text('No data available')
+                if impact_results_df.empty:
+                    st.text('No data available')
 
-            else:
-                impact_results_df_w_gen_names = impact_results_df.merge(cache.generation_types, left_on='ElectricityGenerationTypeId',
-                                                                right_on='Id')
-                del impact_results_df
-                impact_results_df_w_gen_names.drop(['Id'], axis=1, inplace=True)
-                impact_results_df_w_gen_names.rename({'Name': 'ElectricityType'}, axis=1, inplace=True)
-                impact_unit = impact_results_df_w_gen_names.ImpactCategoryUnit.iloc[0]
-                functional_unit = impact_results_df_w_gen_names.PerUnit.iloc[0]
-                st.text(f"Total impact of {region_code} on {impact_category_name} over {start_date} - {end_date}")
-                st.area_chart(impact_results_df_w_gen_names, x='DateStamp', y=f'EnvironmentalImpact',color='ElectricityType',x_label='Datestamp',y_label=f'{impact_category_name[:15]} ({impact_unit})')
-                with st.expander('Show impact data'):
-                    st.dataframe(impact_results_df_w_gen_names)
+                else:
+                    impact_results_df_w_gen_names = impact_results_df.merge(cache.generation_types, left_on='ElectricityGenerationTypeId',
+                                                                    right_on='Id')
+                    del impact_results_df
+                    impact_results_df_w_gen_names.drop(['Id'], axis=1, inplace=True)
+                    impact_results_df_w_gen_names.rename({'Name': 'ElectricityType'}, axis=1, inplace=True)
+                    impact_unit = impact_results_df_w_gen_names.ImpactCategoryUnit.iloc[0]
+                    functional_unit = impact_results_df_w_gen_names.PerUnit.iloc[0]
+                    st.text(f"Total impact of {region_code} on {impact_category_name} over {start_date} - {end_date}")
+                    st.area_chart(impact_results_df_w_gen_names, x='DateStamp', y=f'EnvironmentalImpact',color='ElectricityType',x_label='Datestamp',y_label=f'{impact_category_name[:15]} ({impact_unit})')
+                    with st.expander('Show impact data'):
+                        st.dataframe(impact_results_df_w_gen_names)
 
 
 if __name__ == '__main__':
